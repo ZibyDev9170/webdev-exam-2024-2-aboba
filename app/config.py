9461f93768710be7f48d7eb8f3ed315978b28d21
ZibyDev9071 <nik.yashin.03@inbox.ru>
2024-06-17 00:36:26 +0300

    Delete random secret key

--- app/config.py
+++ app/config.py
@@ -5,5 +5,4 @@ class Config:
     SESSION_COOKIE_NAME = '6342eb430febed6def4d4063a2fa717907fb4911143de1891c59dca9e0519bd8COOKIE'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///library.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.urandom(24)
     UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'media', 'images')
