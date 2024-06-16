60950bc482e75c8dbeba6e1f391e74c4e876a640
ZibyDev9071 <nik.yashin.03@inbox.ru>
2024-06-17 00:26:50 +0300

    Adding secret key

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
+    SECRET_KEY = '6342eb430febed6def4d4063a2fa717907fb4911143de1891c59dca9e0519bd8'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///library.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(24)
